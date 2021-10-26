1010ea0c4c4935d17d09b290210fcad5f4c87909
MrGeet <aldarbatomunkuev@gmail.com>
2021-10-26 16:42:55 +1300

    start

--- src/App.js
+++ src/App.js
@@ -1,21 +1,5 @@
-import { useState } from 'react';
-import Dropdown from './components/Dropdown/Dropdown';
-import GlobalStyle, { Container, Header, Row } from './globalStyles';
-
 function App() {
-	const [currency, setCurrency] = useState('USD');
-
-	return (
-		<>
-			<GlobalStyle />
-			<Container>
-				<Header color="red">itSatori Dropdown Tutorial</Header>
-				<Row justify="center" align="center" mt="4rem">
-					<Dropdown currency={currency} setCurrency={setCurrency} />
-				</Row>
-			</Container>
-		</>
-	);
+	return <></>;
 }
 
 export default App;
